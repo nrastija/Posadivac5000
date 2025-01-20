04fd35e875c97dfbcc1a05f00518348bf047096d
nrastija <niko.rastija@gmail.com>
2025-01-20 15:10:40 +0100

    Implement test enviroment for testing DHT22 sensor and its value reading

--- Posadivac_5000/src/main.cpp
+++ Posadivac_5000/src/main.cpp
@@ -1,11 +1,36 @@
 #include <Arduino.h>
+#include <DHT.h>
 
-#define DHT22_PIN 4
+#define DHTPIN 4
+#define DHTTYPE DHT22
+
+//Inicijalizacija DHT senzora
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-
+  Serial.begin(115200);
+  dht.begin();
+  Serial.println("DHT22 test!");
 }
 
 void loop() {
+  delay(2000);
+  
+  float temperatura_zraka = dht.readTemperature();
+  float vlaznost_zraka = dht.readHumidity();
+
+  if (isnan(temperatura_zraka) || isnan(vlaznost_zraka)) {
+    Serial.println("Greska pri čitanju vrijednosti sa DHT22 senzora!");
+    return;
+  }
+
+  Serial.print("Temperatura: ");
+  Serial.print(temperatura_zraka);
+  Serial.println(" °C");
+
+  Serial.print("Vlažnost: ");
+  Serial.print(vlaznost_zraka);
+  Serial.println(" %");
+
 }
 
